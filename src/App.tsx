7cd3d97f86a41ed1104239147f5f8842692bf9c6
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-05 18:52:10 +0900

    feat: GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 
+import GlobalStyle from "./globalStyle";
 import router from "./routes/route";
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <GlobalStyle />
     </>
   );
 }
