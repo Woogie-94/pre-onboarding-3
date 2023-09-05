3a3d01789b09c57514edc0ef2fcdff0c5f2f30f4
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-05 20:20:40 +0900

    feat: Main 화면 구성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,34 @@
+import { styled } from "styled-components";
+
+import SearchInput from "../components/SearchInput";
+import useInput from "../hooks/useInput";
+
 const Main = () => {
-  return <>Main</>;
+  const { value, onChange } = useInput("");
+
+  return (
+    <Wrapper>
+      <Title>
+        국내 모든 임상시험 검색하고 <br /> 온라인으로 참여하기
+      </Title>
+      <SearchInput value={value} onChange={onChange} />
+    </Wrapper>
+  );
 };
 
 export default Main;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 80px 0 160px;
+  background-color: #cae9ff;
+`;
+
+const Title = styled.h2`
+  font-size: 2.125rem;
+  color: #1e2025;
+  text-align: center;
+  margin-bottom: 40px;
+`;
