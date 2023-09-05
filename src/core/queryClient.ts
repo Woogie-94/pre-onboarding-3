5f22c0a7691d2c14e4ba7f4cf11090bdf1d9b686
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-05 23:08:15 +0900

    feat: queryClient & useQuery 구현
    캐싱 기능을 가지고 있는 query를 구현했습니다. cacheTime을 통해 캐싱 시간을 정할 수 있습니다.
    
    - Query 구현: key, data, option 등을 관리하는 class입니다.
    - QueryClient 구현: Query를 캐싱하는 공간입니다.
    - useQuery 구현: React와 QueryClient의 Query들을 동기화 시킵니다. useQuery를 통해 HTTP Response를 Query에 저장할 수 있습니다.

---
+++ src/core/queryClient.ts
@@ -0,0 +1,86 @@
+import Query from "./query";
+import { QueryFn, QueryKey, QueryOptions } from "./queryType";
+
+class QueryClient {
+  states: Query[];
+  listeners: Set<() => void>;
+
+  constructor() {
+    this.states = [];
+    this.listeners = new Set();
+  }
+
+  getQueryData<TData = unknown>(key: QueryKey) {
+    return this.find<TData>(key);
+  }
+
+  setQueryData<TData = unknown>(key: QueryKey, data: TData, options?: QueryOptions) {
+    if (!this.isStateEmpty) {
+      this.states = [
+        ...this.states.map(state => {
+          if (state.key === key) {
+            state.setData(data);
+          }
+
+          return state;
+        }),
+        new Query({ key, data, options }),
+      ];
+    } else {
+      this.states = [new Query({ key, data, options })];
+    }
+  }
+
+  updateQuerys() {
+    this.states = this.states.filter(state => !state.isExpiredQuery);
+  }
+
+  find<TData = unknown>(key: QueryKey): Query<TData> | undefined {
+    if (typeof key === "object") {
+      return this.states.find(state => {
+        if (typeof state.key === "object") {
+          return state.key.every(item => key.find(k => item === k));
+        }
+        return false;
+      }) as Query<TData>;
+    } else {
+      return this.states.find(state => state.key === key) as Query<TData>;
+    }
+  }
+
+  async fetch<TData = unknown>(key: QueryKey, queryFn: QueryFn<TData>, options?: QueryOptions) {
+    if (this.find(key)) {
+      return;
+    }
+
+    try {
+      const result = await queryFn();
+      this.setQueryData(key, result, options);
+    } catch (error) {
+      if (!this.isStateEmpty) {
+        this.states = this.states.map(state => {
+          if (state.key === key) {
+            state.setError(error);
+          }
+          return state;
+        });
+      } else {
+        this.states = [new Query({ key, data: undefined, error })];
+      }
+    }
+
+    this.listeners.forEach(listener => listener());
+  }
+
+  subscribe(listener: () => void) {
+    this.updateQuerys();
+    this.listeners.add(listener);
+    return () => this.listeners.delete(listener);
+  }
+
+  get isStateEmpty() {
+    return this.states.length === 0;
+  }
+}
+
+export default QueryClient;
