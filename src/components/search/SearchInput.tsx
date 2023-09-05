4159cd35d5f0f3e4c8f8c7e4e20431538f893d5e
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-06 02:26:41 +0900

    fix: 검색창에 value가 있을 때 placeholder가 겹치는 문제 해결

--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -18,7 +18,7 @@ const SearchInput = ({ value, isFocus, onChange, onFocus }: Props) => {
 
   return (
     <Wrapper $isFocus={isFocus}>
-      {!isFocus && (
+      {!isFocus && !value && (
         <Placeholder>
           <SearchSvg width="16" height="16" fill="#A7AFB7" />
           <p>질환명을 입력해주세요.</p>
